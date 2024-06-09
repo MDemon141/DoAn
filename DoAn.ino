cfb24f17a65c4224ff736edaf8eeb2beb9f600d9
NSM <115996542+MDemon141@users.noreply.github.com>
2024-06-09 20:29:11 +0700

    Update DoAn.ino

--- DoAn.ino
+++ DoAn.ino
@@ -6,8 +6,8 @@
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
-#define API_KEY "AIzaSyDMaokD3SIFf-FZ--pAu8bhgNLmELBeFR4"
-#define DATABASE_URL "https://doan-141-default-rtdb.firebaseio.com"
+#define API_KEY "ABCDEFGH"
+#define DATABASE_URL "ABCDEFGH.COM"
 
 FirebaseData fbdo;
 FirebaseData stream;
